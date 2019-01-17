41bd71c5684de41e216c006cbe7f25a5a3710874
Ishup Khan <ishupk@metropolia.fi>
2019-01-17 16:08:57 +0200

    task1 with branch http-a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
