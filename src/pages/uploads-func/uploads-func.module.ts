a3ac9d103595883be0c653fdaa3409950cda7624
Ishup Khan <ishupk@metropolia.fi>
2019-02-04 11:27:41 +0200

    some modifications on form

---
+++ src/pages/uploads-func/uploads-func.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { UploadsFuncPage } from './uploads-func';
+
+@NgModule({
+  declarations: [
+    UploadsFuncPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(UploadsFuncPage),
+  ],
+})
+export class UploadsFuncPageModule {}
