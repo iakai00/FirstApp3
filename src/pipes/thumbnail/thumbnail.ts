a4150db748e27c56e4334dd0ee9719f0b27854ab
Ishup Khan <ishupk@metropolia.fi>
2019-02-14 15:27:31 +0200

    added mextra for uploads

--- src/pipes/thumbnail/thumbnail.ts
+++ src/pipes/thumbnail/thumbnail.ts
@@ -9,22 +9,22 @@ import { Media } from '../../interfaces/pic';
  */
 @Pipe({
   name: 'thumbnail',
-  //pure: false,
+  // pure: false,
 })
 export class ThumbnailPipe implements PipeTransform {
   /**
    * Takes a value and makes it lowercase.
    */
-  private thumbnail = '';
-  private cachedId;
+  // private thumbnail = '';
+  // private cachedId;
 
   constructor(private mediaProvider: MediaProvider) {
 
   }
 
   async transform(id: number, ...args) {
-    console.log(args);
-    //impure version
+    // console.log(args);
+    // impure version
     /*
     if (this.cachedId !== id) {
       this.cachedId = id;
@@ -48,7 +48,7 @@ export class ThumbnailPipe implements PipeTransform {
     return this.thumbnail;
     */
 
-    //pure version
+    // pure version
     return new Promise((resolve, reject) => {
       this.mediaProvider.getSingleMedia(id).subscribe((response: Media) => {
         switch (args[0]) {
